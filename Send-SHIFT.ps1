37ca34b86edd0c3f498d053041190b6dc48b5432
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2025-05-07 13:37:08 -0400

    Add ps1 script to send SHIFT keystroke at intervals

---
+++ Send-SHIFT.ps1
@@ -0,0 +1,14 @@
+# Script to send "shift" key stroke at interval
+
+# Add the necessary assembly
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create a new form (required to use SendKeys)
+$form = New-Object System.Windows.Forms.Form
+
+while ($true) {
+    # Send the Shift key
+    [System.Windows.Forms.SendKeys]::SendWait("+")
+    Write-Host "Sent keystroke"
+    Start-Sleep -Seconds 300
+}
